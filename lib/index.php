dba3035ecc531c354111f24813c970290e04ed7d
Elanis <elanis@hotmail.com>
2017-09-15 15:32:19 +0200

    Fix security breach

--- lib/index.php
+++ lib/index.php
@@ -1,6 +1,5 @@
 <?php
 header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
-echo "<h1>Error 404 Not Found</h1>";
-echo "The page that you have requested could not be found.";
+readfile("../errors/404.html");
 exit();
 ?>
