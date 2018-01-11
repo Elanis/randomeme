9d9e9ebd34889708bad98ff4650e597f3bc18306
Elanis <elanis@hotmail.com>
2018-01-11 20:06:48 +0100

    Fixing opengraph metas

--- index.php
+++ index.php
@@ -113,13 +113,13 @@ if($data !== false && !(empty($data)||!isset($data["link"])||empty($data["link"]
 	<meta property="og:url" content="***REMOVED***/<?php echo $data["link"]; ?>" />
 	<meta property="og:type" content="video.other" />
 	<meta property="og:title" content="Randomeme - <?php echo $data["nom"]; ?>" />
-	<meta property="og:description" content="Get a random and funny meme !" />
 	<meta property="og:image" content="***REMOVED***/img/background.png" />
 	<meta property="og:video:type" content="video/mp4" />
 	<meta property="og:video" content="https://***REMOVED***/randomeme/<?php echo $data["link"]; ?>.mp4" />
 	<meta property="og:video:secure_url" content="https://***REMOVED***/randomeme/<?php echo $data["link"]; ?>.mp4" />
 	<meta property="og:video:width" content="1920" />
 	<meta property="og:video:height" content="1080" />
+	<meta property="og:description" content="Get a random and funny meme !" />
 <?php
 	$adChance = 0;
 }
